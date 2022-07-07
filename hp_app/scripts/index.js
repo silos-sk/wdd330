fdb7cc91fe84cb701ce79465d1dee02676bcb822
silos-sk <97029496+silos-sk@users.noreply.github.com>
2022-07-07 22:56:32 +0100

    module js and css transitions upate

--- hp_app/scripts/index.js
+++ hp_app/scripts/index.js
@@ -1,3 +1,5 @@
+import { getJSON, getRandKey, house } from "./utilities.js";
+
 const hp_btn = document.getElementById("hp_btn");
 const hp_name = document.getElementById("hp_name");
 const hp_result = document.getElementById("hp_result");
@@ -7,21 +9,13 @@ const hp_ancestry = document.getElementById("ancestry");
 const hp_patronus = document.getElementById("patronus");
 const hp_wand = document.getElementById("wand");
 const hp_actor = document.getElementById("actor");
-const side = document.querySelectorAll(".side");
-const result = document.querySelector(".result");
-
-
-
 const hp_house = document.querySelector(".house");
-let date = new Date();
-let currentYear = date.getFullYear();
 
 hp_btn.addEventListener("click", getRandomHp);
 
 // Get Random Harry Potter (HP) Character from API
 function getRandomHp() {
-  fetch("https://hp-api.herokuapp.com/api/characters")
-    .then((res) => res.json())
+  getJSON("https://hp-api.herokuapp.com/api/characters")
     .then((data) => {
       // Call random key generator function
       const randKey = getRandKey(data);
@@ -57,38 +51,21 @@ function getRandomHp() {
           hp_age.textContent = "";
         }
 
-        console.log(yearOfBirth)
-
         if (hp_house.textContent === "SLYTHERIN"){
-          house("rgba(8, 147, 47, 0.545)")
+          house("rgba(8, 147, 47, 0.545)", "side")
           // result.style.borderColor = "rgba(8, 147, 47, 0.3)"
         } else if (hp_house.textContent === "GRYFFINDOR"){
-          house("rgba(147, 5, 20, 0.54)")
+          house("rgba(147, 5, 20, 0.54)", "side")
         } else if (hp_house.textContent === "HUFFLEPUFF"){
-          house("rgba(242, 226, 5, 0.54)")
+          house("rgba(242, 226, 5, 0.54)", "side")
           // result.style.borderColor = "rgba(242, 226, 5, 0.3)"
         } else if (hp_house.textContent === "RAVENCLAW"){
-          house("rgba(4,19,85, 0.54)")
+          house("rgba(4,19,85, 0.54)", "side")
           // result.style.borderColor = "rgba(3, 12, 145, 0.3)"
         } else {
-          house("rgb(202, 138, 36, 0.54)")
+          house("rgb(202, 138, 36, 0.54)", "side")
         }
-
-        function house(color) {
-          var elements = document.getElementsByClassName('side'); // get all elements
-          for(var i = 0; i < elements.length; i++){
-            elements[i].style.backgroundColor = color;
-            elements[i].style.borderColor = color;
-          }
-        }
-
         
       }
     });
-}
-
-// Generate random key number
-function getRandKey(data) {
-  let randKey = parseInt(Math.floor(Math.random() * data.length + 1));
-  return randKey;
 }
---
+++ hp_app/scripts/utilities.js
@@ -0,0 +1,36 @@
+export function getJSON(url) {
+    return fetch(url)
+        .then(function(response) {
+            if (!response.ok) {
+                throw Error(response.statusText);
+            } else {
+                return response.json();
+            }
+        })
+        .catch(function(error) {
+            console.log(error);
+        });
+}
+
+// Generate random key number
+export function getRandKey(data) {
+    let randKey = parseInt(Math.floor(Math.random() * data.length + 1));
+    return randKey;
+  }
+
+export function toMonthName(monthNumber) {
+    const date = new Date();
+    date.setMonth(monthNumber - 1);
+  
+    return date.toLocaleString('en-US', {
+      month: 'long',
+    });
+  }
+
+export function house(color, className) {
+    var elements = document.getElementsByClassName(className); // get all elements
+    for(var i = 0; i < elements.length; i++){
+      elements[i].style.backgroundColor = color;
+      elements[i].style.borderColor = color;
+    }
+  }
